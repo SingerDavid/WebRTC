7a875c2ed3e53d8239c39a7c9d9db5ee607f051d
SingerDavid <dsinger1@hawk.iit.edu>
2021-10-07 13:20:07 -0500

    Adjust minor things before requesting pull from desktop and pushing laptop changes to desktop

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,11 +1,26 @@
 'use strict';
 
-/* Now we have to ask the browser to request for video/user media */
 const $self = {
   constraints: { audio: false, video: true }
 };
 
-/* automatically makes video play. This sets up a stream between self */
+requestUserMedia($self.constraints);
+
+async function requestUserMedia(constraints) {
+  const video = document.getElementById('self');
+  $self.stream = await navigator.mediaDevices.getUserMedia(constraints);
+  video.srcObject = $self.stream;
+}
+
+/*
+'use strict';
+
+/* Now we have to ask the browser to request for video/user media
+const $self = {
+  constraints: { audio: false, video: true }
+};
+
+/* automatically makes video play. This sets up a stream between self
 requestUserMedia($self.constraints);
 
 async function requestUserMedia(constraints) {
@@ -14,3 +29,4 @@ async function requestUserMedia(constraints) {
     .getUserMedia(constraints);
   video.srcObject = $self.stream;
 }
+*/
